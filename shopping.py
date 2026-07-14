7c474f73c29690e525e15870a8559dee86b40578
evaguia <eva03g09@gmail.com>
2026-07-14 22:15:34 +0200

    Week 3: Add stock system

--- shopping.py
+++ shopping.py
@@ -1,17 +1,34 @@
 # Shopping cart
 
-articles = ["🧸", "💿", "📷", "💽", "📃", "📓"]
+articles = {
+    "🧸": 3,
+    "💿": 3,
+    "📷": 3,
+    "💽": 3,
+    "📃": 3,
+    "📓": 3
+}
 
 def add_to_cart(to_add):
-    cart.append(to_add)
-    print (f"{to_add} added to cart.")
+    if articles[to_add] != 0:
+      cart.append(to_add)
+      print (f"{to_add} added to cart.")
+      articles[to_add] -= 1
+    else:
+        print(f"{to_add} is sold out !")
+
 
 def remove(to_remove):
-    try: 
+    try:  
         cart.remove(to_remove)
+        articles[to_remove] += 1
         print(f"{to_remove} removed from cart.")
     except ValueError:
         print(f"{to_remove} is not in your cart...")
+
+def check_stock():
+    for article, stock in articles.items():
+        print(f"{article} - {stock}")
 
 
 cart = []
@@ -23,12 +40,13 @@ while True:
     print("Enter 1) to add an element")
     print("Enter 2) to remove an element")
     print("Enter 3) to display cart")
-    print("Enter 4) to quit")
+    print("Enter 4) to check stock")
+    print("Enter 5) to quit")
 
     choice = input("Enter: ")
 
     if choice == "1":
-        print(articles)
+        print(list(articles))
         to_add = input("Enter an article to add: ")
         if to_add not in articles:
             print(f"{to_add} is not available.")
@@ -48,9 +66,11 @@ while True:
             print("You cart is empty.")
         else:
             print(f"Your cart is composed of {cart}")
-
-
+    
     elif choice == "4":
+        check_stock()
+
+    elif choice == "5":
         print("Quitting")
         break
 
